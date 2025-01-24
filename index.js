ec5fcd8ca34660368b7226a58718c9dae550a4ba
El-Pugi <102178894+El-Pugi@users.noreply.github.com>
2025-01-24 15:22:17 +0200

    3 commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 let persons = [
