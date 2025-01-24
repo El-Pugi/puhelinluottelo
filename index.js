feedd656f07a8db3850dc94cad544c8a34f54cb7
El-Pugi <102178894+El-Pugi@users.noreply.github.com>
2025-01-24 14:05:35 +0200

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(cors())
 app.use(express.json())
 
 let persons = [
@@ -81,7 +82,7 @@ app.get('/info', (request, response) => {
     <p>${date}</p>`)
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
     app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
